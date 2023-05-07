01c0fc6b2e6c26e8c8034627e5481c4b296c3240
Mikita Maliauka <mikita.maliauka@gmail.com>
2023-05-08 00:05:24 +0300

    Add AppContext

---
+++ ClothesApp/ClothesApp/ClothesAppContext.cs
@@ -0,0 +1,64 @@
+﻿using ClothesApp.Models;
+using ClothesApp.ModelsConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClothesApp;
+
+public class ClothesAppContext : DbContext
+{
+    public ClothesAppContext()
+    {
+    }
+
+    public ClothesAppContext(DbContextOptions<ClothesAppContext> options)
+        : base(options)
+    {
+
+    }
+
+    public virtual DbSet<Address> Addresses { get; set; }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Media> Media { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderItem> OrdersItems { get; set; }
+
+    public virtual DbSet<OrdersTransaction> OrdersTransactions { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Review> Reviews { get; set; }
+
+    public virtual DbSet<Section> Sections { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .HasPostgresEnum("order_status_type", new[] { "inreview", "indelivery", "completed" })
+            .HasPostgresEnum("user_type", new[] { "admin", "customer" });
+
+        modelBuilder.ApplyConfiguration(new AddressConfiguration());
+        modelBuilder.ApplyConfiguration(new BrandConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new MediaConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderTransactionConfiguration());
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        modelBuilder.ApplyConfiguration(new ReviewConfiguration());
+        modelBuilder.ApplyConfiguration(new SectionConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+    }
+}
