4b31cd2e7018a16a40dd737fc798583da58e568c
Mikita Maliauka <mikita.maliauka@gmail.com>
2023-05-16 00:07:54 +0300

    fixed remarks

--- ClothesApp/ClothesApp/Program.cs
+++ ClothesApp/ClothesApp/Program.cs
@@ -1,225 +1,140 @@
-﻿using System.Collections;
-using ClothesApp;
+﻿using ClothesApp;
 using ClothesApp.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
-using var db = new ClothesAppContext();
+await using var db = new ClothesAppContext();
 
-Console.WriteLine("-----------------LAZY-----------------------");
-Console.WriteLine("get_products_by_brand_id");
-var products = await db.Products
-    .Where(p => p.BrandId == 1)
-    .ToListAsync();
-    
-Console.WriteLine("after query string");
+var productsByBrandIdLazy = await GetProductsByBrandIdLazy(db, 1);
+GetBrandsWithProductAmountLazy(db);
+var productsBySectionCategoryLazy = await GetProductsBySectionCategoryLazy(db, 1, 1);
+var completedOrdersByProductIdLazy = await GetCompletedOrdersByProductIdLazy(db, 1);
+var reviewsForProductLazy = await GetAllReviewsForProductLazy(db, 1);
 
-foreach (var p in products)
+var productsByBrandId = await GetProductsByBrandId(db, 1);
+GetBrandsWithProductsAmount(db);
+var productsBySectionCategory = await GetProductsBySectionCategory(db, 1, 1);
+var completedOrdersByProductId = await GetCompletedOrdersByProductId(db, 1);
+var reviewsForProduct = await GetAllReviewsForProduct(db, 1);
+
+var brand = await db.Brands.FindAsync((long)4);
+brand!.Name = "mark formelle";
+
+await using (var context = new ClothesAppContext())
 {
-    Console.WriteLine($"id: {p.Id}, " +
-                      $"brand_id: {p.BrandId}, " +
-                      $"brand_name: {p.Brand.Name}, " +
-                      $"category_id: {p.CategoryId}," +
-                      $"name: {p.Name}," +
-                      $"price: {p.Price}," +
-                      $"quantity: {p.Quantity}," +
-                      $"created at: {p.CreatedAt}");
-}
-    
-Console.WriteLine("---------------------------------------------------------");
-    
-Console.WriteLine("get_brands_with_products_amount");
-var brands = await db.Products
-    .GroupBy(b => b.BrandId, (b, p) => 
-        new
-        {
-            BrandId = b,
-            ProductCount = p.Count()
-        })
-    .OrderByDescending(p => p.ProductCount)
-    .ToListAsync();
-
-Console.WriteLine("after query string");
-    
-foreach(var b in brands)
-{
-    Console.WriteLine($"brand_id: {b.BrandId}, " +
-                      $"product_count: {b.ProductCount}");
+    context.Entry(brand).CurrentValues.SetValues(brand);
+    context.Update(brand);
+    await context.SaveChangesAsync();
 }
 
-Console.WriteLine("---------------------------------------------------------");
-    
-Console.WriteLine("get_products_by_section_and_category");
-var productsBySectionCategory = await db.Products
-    .Where(p => p.Category.Id == 2 && p.Category.Sections.Any(s => s.Id == 1))
-    .ToListAsync();
-     
-Console.WriteLine("after query string");
-
-foreach (var p in productsBySectionCategory)
+async Task<List<Product>> GetProductsByBrandIdLazy(ClothesAppContext context, long brandId)
 {
-    Console.WriteLine($"id: {p.Id}, " +
-                      $"brand_id: {p.BrandId}, " +
-                      $"brand_name: {p.Brand.Name}, " +
-                      $"category_id: {p.CategoryId}," +
-                      $"category_name: {p.Category.Name}," +
-                      $"name: {p.Name}," +
-                      $"price: {p.Price}," +
-                      $"quantity: {p.Quantity}," +
-                      $"created at: {p.CreatedAt}");
-}
-     
-Console.WriteLine("---------------------------------------------------------");
-    
-Console.WriteLine("get_completed_orders_by_product_id");
-var orders = await db.Orders
-    .Where(o => o.OrderStatus == OrderStatusType.Completed && o.OrdersItems.Any(p => p.ProductId == 1))
-    .OrderByDescending(o => o.CreatedAt)
-    .ToListAsync();
+    var result = await context.Products
+        .Where(p => p.BrandId == brandId)
+        .ToListAsync();
 
-Console.WriteLine("after query string");
-
-foreach (var o in orders)
-{
-    Console.WriteLine($"id: {o.Id}, " +
-                      $"user_name: {o.User.FirstName + " " + o.User.LastName}, " +
-                      $"price: {o.Price}, " +
-                      $"address_id: {o.Address.AddressLine}, " +
-                      $"created_at: {o.CreatedAt}, " +
-                      $"status: {o.OrderStatus}");
-}
-    
-Console.WriteLine("---------------------------------------------------------");
-    
-Console.WriteLine("get_all_reviews");
-var reviews = await db.Reviews
-    .Where(r => r.ProductId == 1 && r.User.Id == r.UserId)
-    .ToListAsync();
-
-Console.WriteLine("after query string");
-
-foreach (var r in reviews)
-{
-    Console.WriteLine($"rating: {r.Rating}," +
-                      $"title: {r.Title}," +
-                      $"comment: {r.Comment}," +
-                      $"name: {r.User.FirstName + " " + r.User.LastName}");
+    return result;
 }
 
-Console.WriteLine("-----------------------------EAGER-------------------------------------");
-
-Console.WriteLine("get_products_by_brand_id");
-var productse = db.Products
-    .Include(b => b.Brand)
-    .Where(p => p.BrandId == 1);
-
-Console.WriteLine("after query string");
-
-foreach (var p in productse)
+void GetBrandsWithProductAmountLazy(ClothesAppContext context)
 {
-    Console.WriteLine($"id: {p.Id}, " +
-                      $"brand_id: {p.BrandId}, " +
-                      $"brand_name: {p.Brand.Name}, " +
-                      $"category_id: {p.CategoryId}," +
-                      $"name: {p.Name}," +
-                      $"price: {p.Price}," +
-                      $"quantity: {p.Quantity}," +
-                      $"created at: {p.CreatedAt}");
-}
-    
-Console.WriteLine("---------------------------------------------------------");
-    
-Console.WriteLine("get_brands_with_products_amount");
-var brandse = db.Products
-    .GroupBy(b => b.BrandId, (b, p) => 
-        new
-        {
-            BrandId = b,
-            ProductCount = p.Count()
-        })
-    .OrderByDescending(p => p.ProductCount);
-    
-
-Console.WriteLine("after query string");
-
-foreach(var b in brandse)
-{
-    Console.WriteLine($"brand_id: {b.BrandId}, " +
-                      $"product_count: {b.ProductCount}");
+    var result = context.Products
+        .GroupBy(b => b.BrandId, (b, p) =>
+            new
+            {
+                BrandId = b,
+                ProductCount = p.Count()
+            })
+        .OrderByDescending(p => p.ProductCount)
+        .ToListAsync();
 }
 
-Console.WriteLine("---------------------------------------------------------");
-    
-Console.WriteLine("get_products_by_section_and_category");
-var productsBySectionCategorye = db.Products
-    .Include(b => b.Brand)
-    .Include(c => c.Category)
-    .Include(s => s.Category.Sections)
-    .Include(sc => sc.Category.SectionCategories)
-    .Where(p => p.Category.Id == 2 && p.Category.Sections.Any(s => s.Id == 1));
-    
-
-Console.WriteLine("after query string");
-
-foreach (var p in productsBySectionCategorye)
+async Task<List<Product>> GetProductsBySectionCategoryLazy(ClothesAppContext context, long sectionId, long categoryId)
 {
-    Console.WriteLine($"id: {p.Id}, " +
-                      $"brand_id: {p.BrandId}, " +
-                      $"brand_name: {p.Brand.Name}, " +
-                      $"category_id: {p.CategoryId}," +
-                      $"category_name: {p.Category.Name}," +
-                      $"name: {p.Name}," +
-                      $"price: {p.Price}," +
-                      $"quantity: {p.Quantity}," +
-                      $"created at: {p.CreatedAt}");
-}
-     
-Console.WriteLine("---------------------------------------------------------");
-    
-Console.WriteLine("get_completed_orders_by_product_id");
-var orderse = db.Orders
-    .Include(o => o.OrdersItems)
-    .Include(a => a.Address)
-    .Include(u => u.User)
-    .Where(o => o.OrderStatus == OrderStatusType.Completed && o.OrdersItems.Any(p => p.ProductId == 1))
-    .OrderByDescending(o => o.CreatedAt);
+    var result = await context.Products
+        .Where(p => p.Category.Id == categoryId 
+                    && p.Category.Sections.Any(s => s.Id == sectionId))
+        .ToListAsync();
 
-Console.WriteLine("after query string");
-
-foreach (var o in orderse)
-{
-    Console.WriteLine($"id: {o.Id}, " +
-                      $"user_name: {o.User.FirstName + " " + o.User.LastName}, " +
-                      $"price: {o.Price}, " +
-                      $"address_id: {o.Address.AddressLine}, " +
-                      $"created_at: {o.CreatedAt}, " +
-                      $"status: {o.OrderStatus}");
-}
-    
-Console.WriteLine("---------------------------------------------------------");
-    
-Console.WriteLine("get_all_reviews");
-var reviewse = db.Reviews
-    .Include(u => u.User)
-    .Where(r => r.ProductId == 1 && r.User.Id == r.UserId);
-
-Console.WriteLine("after query string");
-
-foreach (var r in reviewse)
-{
-    Console.WriteLine($"rating: {r.Rating}," +
-                      $"title: {r.Title}," +
-                      $"comment: {r.Comment}," +
-                      $"name: {r.User.FirstName + " " + r.User.LastName}");
+    return result;
 }
 
-var brand = new Brand(){Name = "mark formelle" };
-
-using (var context = new ClothesAppContext())
+async Task<List<Order>> GetCompletedOrdersByProductIdLazy(ClothesAppContext context, long productId)
 {
-    context.Add<Brand>(brand);
-    context.SaveChanges();
-    foreach (var tmp in context.Brands)
-    {
-        Console.WriteLine($"brand id: {tmp.Id}, name: {tmp.Name}");   
-    }
+    var result = await context.Orders
+        .Where(o => o.OrderStatus == OrderStatusType.Completed 
+                    && o.OrdersItems.Any(p => p.ProductId == productId))
+        .OrderByDescending(o => o.CreatedAt)
+        .ToListAsync();
+    
+    return result;
+}
+
+async Task<List<Review>> GetAllReviewsForProductLazy(ClothesAppContext context, long productId)
+{
+    var result = await context.Reviews
+        .Where(r => r.ProductId == productId && r.User.Id == r.UserId)
+        .ToListAsync();
+    
+    return result;
+}
+    
+async Task<List<Product>> GetProductsByBrandId(ClothesAppContext context, long brandId)
+{
+    var result = await context.Products
+        .Include(b => b.Brand)
+        .Where(p => p.BrandId == brandId)
+        .ToListAsync();
+    
+    return result;
+}
+
+void GetBrandsWithProductsAmount(ClothesAppContext context)
+{
+    var result = context.Products
+        .GroupBy(b => b.BrandId, (b, p) =>
+            new
+            {
+                BrandId = b,
+                ProductCount = p.Count()
+            })
+        .OrderByDescending(p => p.ProductCount)
+        .ToListAsync();
+}
+
+async Task<List<Product>> GetProductsBySectionCategory(ClothesAppContext context, long sectionId, long categoryId)
+{
+    var result = await context.Products
+        .Include(b => b.Brand)
+        .Include(c => c.Category)
+        .Include(s => s.Category.Sections)
+        .Include(sc => sc.Category.SectionCategories)
+        .Where(p => p.Category.Id == categoryId && p.Category.Sections.Any(s => s.Id == sectionId))
+        .ToListAsync();
+    
+    return result;
+}
+
+async Task<List<Order>> GetCompletedOrdersByProductId(ClothesAppContext context, long productId)
+{
+    var result = await context.Orders
+        .Include(o => o.OrdersItems)
+        .Include(a => a.Address)
+        .Include(u => u.User)
+        .Where(o => o.OrderStatus == OrderStatusType.Completed 
+                    && o.OrdersItems.Any(p => p.ProductId == productId))
+        .OrderByDescending(o => o.CreatedAt)
+        .ToListAsync();
+
+    return result;
+}
+
+async Task<List<Review>> GetAllReviewsForProduct(ClothesAppContext context, long productId)
+{
+    var result = await context.Reviews
+        .Include(u => u.User)
+        .Where(r => r.ProductId == productId && r.User.Id == r.UserId)
+        .ToListAsync();
+
+    return result;
 }
