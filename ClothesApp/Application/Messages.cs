c43c98498a4d7c8d0cebc1d865d101b80d8daca5
Mikita Maliauka <mikita.maliauka@gmail.com>
2023-06-09 14:43:06 +0300

    update Messages.cs

--- ClothesApp/Application/Messages.cs
+++ ClothesApp/Application/Messages.cs
@@ -1,4 +1,4 @@
-namespace Application.Exceptions;
+namespace Application;
 
 public class Messages
 {
@@ -17,4 +17,15 @@ public class Messages
     public const string CategoryDeleteConstraint = "You can't delete a category that is a parent category";
    
     public const string ProductNameUniqueConstraint = "Product with this name already exists";
+    
+    public const string ProductAvailable = "Product available";
+    
+    public const string ProductOutOfStock = "Product is out of stock";
+   
+    public const string SectionUniqueConstraint = "Section name must be unique";
+
+    public const string ParentCategoryConstraint = "No parental category exists";
+    
+    public const string SectionDeleteConstraint = "You can't delete a section that have related categories";
+
 }
