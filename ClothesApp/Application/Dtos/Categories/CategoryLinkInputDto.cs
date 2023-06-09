10d0de4d7a65f0639b099b1a60911505f9bd70d4
Mikita Maliauka <mikita.maliauka@gmail.com>
2023-06-09 14:47:01 +0300

    update dtos

---
+++ ClothesApp/Application/Dtos/Categories/CategoryLinkInputDto.cs
@@ -0,0 +1,6 @@
+namespace Application.Dtos.Category;
+
+public class CategoryLinkInputDto
+{
+    public string Name { get; set; }
+}
---
+++ ClothesApp/Application/Dtos/OrderItems/OrderItemInputDto.cs
@@ -0,0 +1,8 @@
+namespace Application.Dtos.OrderItems;
+
+public class OrderItemInputDto
+{
+    public long ProductId { get; set; }
+
+    public long Quantity { get; set; }
+}
