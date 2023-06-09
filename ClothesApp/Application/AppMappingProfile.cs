82b59e5a3e99a7ce6a8eca383e56d2f4532e2491
Mikita Maliauka <mikita.maliauka@gmail.com>
2023-06-09 14:44:14 +0300

    update AppMappingProfile.cs

--- ClothesApp/Application/AppMappingProfile.cs
+++ ClothesApp/Application/AppMappingProfile.cs
@@ -1,6 +1,13 @@
+using Application.Dtos.Addresses;
 using Application.Dtos.Brands;
+using Application.Dtos.Category;
+using Application.Dtos.OrderItems;
+using Application.Dtos.Orders;
+using Application.Dtos.OrderTransactions;
 using Application.Dtos.Products;
 using Application.Dtos.Reviews;
+using Application.Dtos.SectionCategories;
+using Application.Dtos.Sections;
 using Application.Dtos.Users;
 using AutoMapper;
 using Domain.Entities;
@@ -16,9 +23,22 @@ public class AppMappingProfile : Profile
         CreateMap<Review, ReviewDto>().ReverseMap();
         CreateMap<ReviewInputDto, Review>();
         CreateMap<User, UserDto>().ReverseMap();
-        CreateMap<UserInputDto, User>();
+        CreateMap<UserInputInfoDto, User>();
         CreateMap<Order, OrderTransaction>();
         CreateMap<Product, ProductDto>().ReverseMap();
         CreateMap<ProductInputDto, Product>();
+        CreateMap<Order, OrderDto>().ReverseMap();
+        CreateMap<OrderInputDto, Order>();
+        CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+        CreateMap<OrderItemInputDto, OrderItem>();
+        CreateMap<OrderItemInputDto, OrderItemDto>();
+        CreateMap<OrderTransaction, OrderTransactionsDto>().ReverseMap();
+        CreateMap<Category, CategoryDto>().ReverseMap();
+        CreateMap<CategoryInputDto, Category>();
+        CreateMap<Section, SectionDto>().ReverseMap();
+        CreateMap<SectionInputDto, Section>();
+        CreateMap<Address, AddressDto>().ReverseMap();
+        
+        CreateMap<SectionCategory, SectionCategoryDto>().ReverseMap();
     }
 }
