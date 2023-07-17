e3af730cccb30e17351c6a71fc236b28d15f8d0f
Mikita Maliauka <mikita.maliauka@gmail.com>
2023-07-18 01:57:09 +0300

    add cartController

--- ClothesApp/WebAPI/Program.cs
+++ ClothesApp/WebAPI/Program.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using WebAPI.Authentication;
 using WebAPI.Cors;
+using WebAPI.Hubs;
 using WebAPI.Middlewares;
+using WebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,13 +43,28 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy(Policies.Customer, policy =>
         policy.RequireClaim(CustomClaims.UserType, UserType.Customer.ToString()));
 });
-
+builder.Services.AddSignalR();
+// builder.Services.AddHangfire( config => config
+//     .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+//     .UseSimpleAssemblyNameTypeSerializer()
+//     .UseRecommendedSerializerSettings()
+//     .UsePostgreSqlStorage(builder.Configuration.GetConnectionString("DefaultConnection"),
+//         new PostgreSqlStorageOptions()
+//     {
+//         QueuePollInterval = TimeSpan.FromSeconds(5),
+//         SchemaName = "schema",
+//     })
+// );
+// builder.Services.AddHangfireServer();
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<ProductViews>();
+builder.Services.AddScoped<ISignalRService, SignalRService>();
 
 var app = builder.Build();
 
@@ -63,5 +80,10 @@ app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+// app.UseHangfireDashboard();
+// app.MapHangfireDashboard();
+
+app.MapHub<ProductHub>("/products");
 
 app.Run();
