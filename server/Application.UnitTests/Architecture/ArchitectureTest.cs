1afffd3a2ea613b7aeeb060433b14209aede4305
Mikita Maliauka <mikita.maliauka@gmail.com>
2023-07-19 01:06:14 +0300

    add architecture unit tests

---
+++ server/Application.UnitTests/Architecture/ArchitectureTest.cs
@@ -0,0 +1,74 @@
+using NetArchTest.Rules;
+
+namespace UnitTests.Architecture;
+
+public class ArchitectureTest
+{
+    private const string DomainNamespace = "Domain";
+
+    private const string ApplicationNamespace = "Application";
+
+    private const string InfrastructureNamespace = "Infrastructure";
+
+    private const string WebNamespace = "WebAPI";
+
+    [Fact]
+    public void Domain_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        var otherProjects = new[]
+        {
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            WebNamespace,
+        };
+
+        var result = Types
+            .InCurrentDomain()
+            .That()
+            .ResideInNamespace(DomainNamespace)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Application_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            WebNamespace,
+        };
+
+        var result = Types
+            .InCurrentDomain()
+            .That()
+            .ResideInNamespace(ApplicationNamespace)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Infrastructure_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        var otherProjects = new[]
+        {
+            WebNamespace,
+        };
+
+        var result = Types
+            .InCurrentDomain()
+            .That()
+            .ResideInNamespace(ApplicationNamespace)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
